c60245265bab4e90d719d70c78799e7b49f63bf5
eniskastrati <eniskastrati18@gmail.com>
2022-05-12 17:22:30 +0200

    adding connection to db

--- php/AddData.php
+++ php/AddData.php
@@ -12,13 +12,20 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "corona-form"
+    $dbname = "corona-form";
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
     // Check connection
-    if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
     }
 
+    $sql = "INSERT INTO coffeshop-costumers (firstname, lastname, address, date, time)
+    VALUES ('{$fname}', '{$lname}', '{$adress}, '{$date}, '{$time}')";
+
+    if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+    } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
 ?>
