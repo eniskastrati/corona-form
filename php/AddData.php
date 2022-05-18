5e9f3263e3dcfeef057226c4c674ce2b8b3a998a
eniskastrati <eniskastrati18@gmail.com>
2022-05-18 18:57:27 +0200

    adding connection to db

--- php/AddData.php
+++ php/AddData.php
@@ -2,40 +2,47 @@
     if(isset($_POST["submit"])){
         $fname = $_POST["fname"];
         $lname = $_POST["lname"];
+        $email = $_POST["email"];
         $adress = $_POST["adress"];
         $date = $_POST["date"];
         $time = $_POST["time"];
+    }else{
+        echo "No Data Provided from Form";
     }
 
-    $fullInfo = $fname . ";" . $lname . ";" . $adress . ";" . $date . ";" . $time . "\n";
+    //csv format
+    $fullInfoData = $fname . ";" . $lname . ";" . $email . ";". $adress . ";" . $date . ";" . $time . "\n";
+    $fullInfo = $fname . $lname . $email . $adress . $date . $time;
+
 
     //sava the data in a csv file which would be easier for the staff
-    $dataFile = fopen("../data/corona-coffeshop-costumers.csv", "a") or die("Unable to open file!");
+    //$dataFile = fopen("../data/corona-coffeshop-costumers.csv", "a") or die("Unable to open file!");
     
     //write the data into file
-    fwrite($dataFile,$fullInfo);
+    //fwrite($dataFile,$fullInfo);
     //close the file
-    fclose($dataFile);
+    //fclose($dataFile);
 
     //Credentials to database
     $servername = "localhost";
     $username = "root";
-    $password = "";
     $dbname = "corona-form";
+    $password = "";
 
-    //Commenting right now connection and inserting data to databse
-    /*$conn = mysqli_connect($servername, $username, $password, $dbname);
+    // databse connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
     // Check connection
     if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
     }
 
-    $sql = "INSERT INTO coffeshop-costumers (firstname, lastname, address, date, time)
-    VALUES ("{$fname}", "{$lname}", "{$adress}, "{$date}, "{$time}")";
+    $sql = "INSERT INTO costumer-data (firstName, lastName, email, adress, date, time, costumerTrack)
+    VALUES ($fullInfo[0], $fullInfo[1],$fullInfo[2], $fullInfo[3], $fullInfo[4], $fullInfo[5], 'noInfo');
 
     if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
+    $last_id = mysqli_insert_id($conn);
+    echo "New record created successfully. Last inserted ID is: " . $last_id;
     } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }*/
+    }
 ?>
